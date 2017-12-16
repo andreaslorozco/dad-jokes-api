6b08af6c8d57611ff8846d32434ff52dd58694d6
andreaslrorozco <andreaslorozco@gmail.com>
2017-12-16 15:24:09 -0600

    Fixed empty req.body from Postman

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require('express');
 let app = express();
 let db = require('./db');
-
 let JokeController = require('./joke/JokeController');
+let bodyParser = require('body-parser');
+
+
 app.use('/jokes', JokeController);
+app.use(bodyParser.json());
 
 module.exports = app;
--- joke/JokeController.js
+++ joke/JokeController.js
@@ -1,21 +1,28 @@
 let express = require('express');
+let app = express();
 let router = express.Router();
 let bodyParser = require('body-parser');
 
+router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
+
+
 
 let Joke = require('./Joke');
 
 router.post('/', (req, res) => {
-  console.log('will try to connect');
+
   Joke.create({
+
     title: req.body.title,
     text: req.body.text,
     keywords: req.body.keywords
   }, (err, joke) => {
+    console.log(req.body);
     if (err) {
       return res.status(500).send('There was an error adding info to the DB');
     } else {
+      // console.log(req.body);
       res.status(200).send(joke);
     };
   });
