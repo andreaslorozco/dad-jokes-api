15c4baf04205132fa3a6e8700fe6f3d4c8f60841
andreaslrorozco <andreaslorozco@gmail.com>
2017-12-16 15:49:28 -0600

    Added GET joke by ID and DELETE joke by ID"

--- joke/JokeController.js
+++ joke/JokeController.js
@@ -6,9 +6,21 @@ let bodyParser = require('body-parser');
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
-
-
 let Joke = require('./Joke');
+
+// GET /jokes route
+
+router.get('/', (req, res) => {
+  Joke.find({}, (err, jokes) => {
+    if (err) {
+      return res.status(500).send("There was a problem retrieving info from the DB");
+    } else {
+      res.status(200).send(jokes);
+    };
+  });
+});
+
+// POST /jokes route
 
 router.post('/', (req, res) => {
 
@@ -28,12 +40,30 @@ router.post('/', (req, res) => {
   });
 });
 
-router.get('/', (req, res) => {
-  Joke.find({}, (err, jokes) => {
+// GET /jokes single jokes
+
+router.get('/:id', (req, res) => {
+    Joke.findById(req.params.id, (err, joke) => {
+      if (err) {
+        return res.status(500).send("There was a problem finding the joke");
+      } else if (!joke) {
+        return res.status(404).send("No joke found");
+      } else {
+        res.status(200).send(joke);
+      };
+    });
+});
+
+// DELETE A JOKE FROM THE DATABASE
+
+router.delete('/:id', (req, res) => {
+  Joke.findByIdAndRemove(req.params.id, (err, joke) => {
     if (err) {
-      return res.status(500).send("There was a problem retrieving info from the DB");
+      return res.status(500).send("There was a problem deleting the user");
+    } else if (!joke) {
+      return res.status(404).send("No joke found.");
     } else {
-      res.status(200).send(jokes);
+      res.status(200).send(`Joke with title '${joke.title}' was deleted from the database.`);
     };
   });
 });
