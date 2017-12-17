82773360e607d62db69e85bca3054f1f04515e9e
andreaslrorozco <andreaslorozco@gmail.com>
2017-12-17 12:41:30 -0600

    heroku deployment preparation"

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 
-if (NODE_ENV=production) {
+if (NODE_ENV === "production") {
   mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URI}`);
 } else {
   let conf = require('./conf.js');
