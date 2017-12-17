b93585539565c09fa1094411f09307e4207ed214
andreaslrorozco <andreaslorozco@gmail.com>
2017-12-17 12:40:16 -0600

    hide db user and password

--- db.js
+++ db.js
@@ -1,4 +1,9 @@
 let mongoose = require('mongoose');
-let conf = require('./conf.js');
 
-mongoose.connect(`mongodb://${conf.user}:${conf.password}@${conf.uri}`);
+
+if (NODE_ENV=production) {
+  mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URI}`);
+} else {
+  let conf = require('./conf.js');
+  mongoose.connect(`mongodb://${conf.user}:${conf.password}@${conf.uri}`);
+};
